1766620651859ad4a0412ba937425ff2c51cd219
Rodion Shingarev <rodion@Mac-Pro-Rodion.local>
2018-12-30 01:00:42 +0200

    boot

--- boot/clover.sh
+++ boot/clover.sh
@@ -7,14 +7,13 @@ wget -O boot0af https://raw.githubusercontent.com/roddy20/Hackintosh/master/boot
 wget -O boot1f32 https://raw.githubusercontent.com/roddy20/Hackintosh/master/boot/boot1f32
 
 fdisk -l | grep FAT32
-#read -p "Disk? /dev/sd" s
-s="a"
-dd if=/dev/sd$s count=1 of=origMBR
+
+dd if=/dev/sda count=1 of=origMBR
 cp origMBR newMBR
 dd if=boot0af of=newMBR bs=1 count=440 conv=notrunc
-dd if=newMBR of=/dev/sd$s count=1
-dd if=/dev/sd$s1 count=1 of=origbs
+dd if=newMBR of=/dev/sda count=1
+dd if=/dev/sda1 count=1 of=origbs
 cp boot1f32 newbs
 dd if=origbs of=newbs skip=3 seek=3 bs=1 count=87 conv=notrunc
-dd if=newbs of=/dev/sd$s1 count=1
+dd if=newbs of=/dev/sda1 count=1
 echo "done"
